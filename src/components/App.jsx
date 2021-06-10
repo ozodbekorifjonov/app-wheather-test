bf7babc6b24d904730f876cab8cc558bc03ff974
Ozodbek Oripjonov <ooripjonov@exadel.com>
2021-06-10 11:41:50 +0500

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../styles/index.scss";
 import Recipes from "./Recipes";
-import rain from "../images/002-rain.svg";
 
 const App = () => {
     return (
@@ -9,9 +8,8 @@ const App = () => {
             <section className="hero"/>
             <main>
                 <section>
-                    <h1>Oh Hai, React JS</h1>
+                    <h1>Oh Hai, React</h1>
                 </section>
-                <img src={rain} alt="rain" width="250"/>
                 <Recipes/>
             </main>
         </>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
@@ -6,14 +7,26 @@ const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 let mode = "development";
 let target = "web";
 
+const plugins = [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+        template: "./src/index.html"
+    }),
+]
+
 if (process.env.NODE_ENV === "production") {
     mode = "production";
     target = "browserslist";
+} else {
+    plugins.push(new ReactRefreshWebpackPlugin())
 }
 
 module.exports = {
     mode: mode,
     target: target,
+
+    entry: "./src/index.js",
 
     output: {
         path: path.resolve(__dirname, "dist"),
@@ -48,12 +61,7 @@ module.exports = {
         ]
     },
 
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })],
+    plugins: plugins,
 
     resolve: {
         extensions: [".js", ".jsx"]
